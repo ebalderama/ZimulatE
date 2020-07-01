78cdf4cf668d158e4105907a0a9dbf8612e07b79
Vathana To <vathana@mail.fresnostate.edu>
2020-07-01 14:12:52 -0700

    Add DGPD.R

---
+++ R/DGPD.R
@@ -0,0 +1,55 @@
+#' @name DGPD
+#' @title Discrete Generalized Pareto Distribution
+#' @description Density, distribution function, quantile function and random generation for the discrete
+#' generalized Pareto distribution with parameters \code{alpha}, \code{lambda}, and \code{mu}.
+#' @param x,q vector of quantiles
+#' @param p vector of probabilities
+#' @param n number of observations
+#' @param alpha shape parameter
+#' @param lambda scale parameter
+#' @param mu location parameter
+#' @param log,log.p logical; if TRUE, probabilities \code{p} are given as \code{log(p)}.
+#' @param lower.tail logical; if TRUE (default), probabilities are \code{P[X \leq x]}, otherwise,
+#' \code{P[X > x]}.
+#' @return \code{ddgpd} gives the density, \code{pdgpd} gives the distribution function,
+#' \code{qdgpd} gives the quantile function, and \code{rdgpd} generates random deviates.
+#' @export ddgpd pdgpd qdgpd rdgpd
+
+ddgpd <-
+function(x, alpha=1, lambda=1, mu=0, log = FALSE) {
+  (1 + lambda * (x - mu))**-alpha - (1 + lambda * (x - mu + 1))**-alpha
+}
+
+pdgpd <-
+function(q, alpha=1, lambda=1, mu=0, lower.tail = TRUE, log.p = FALSE) {
+  
+  # find where values of q are less than mu
+  zindex <- q < mu
+  
+  p <- 1 - (1 + lambda * (q - mu + 1))**-alpha
+  
+  # replace with 0
+  p[zindex] <- 0
+  
+  if (lower.tail == FALSE) {
+    p <- 1 - p
+  }
+  
+  return(p)
+}
+
+qdgpd <-
+function(p, alpha=1, lambda=1, mu=0, lower.tail = TRUE, log.p = FALSE) {
+  if (lower.tail == FALSE) {
+    p <- 1 - p
+  }
+  if (log.p == TRUE) {
+    p <- exp(p)
+  }
+  ceiling( ((1-p)**(-1/alpha) - 1) / lambda - 1 + mu )
+}
+
+rdgpd <-
+function(n, alpha=1, lambda=1, mu=0,) {
+  
+}
