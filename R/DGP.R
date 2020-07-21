642a7c878e54d3f1d143351127ee66042ce98c7c
Vathana To <vathana@mail.fresnostate.edu>
2020-07-21 09:53:24 -0700

    Change parameter names

--- R/DGP.R
+++ R/DGP.R
@@ -1,13 +1,13 @@
 #' @name DGP
 #' @title Discrete Generalized Pareto Distribution
 #' @description Density, distribution function, quantile function and random generation for the discrete
-#' generalized Pareto distribution with parameters \code{mu}, \code{lambda}, and \code{alpha}.
+#' generalized Pareto distribution with parameters \code{mu}, \code{scale}, and \code{shape}.
 #' @param x,q vector of quantiles
 #' @param p vector of probabilities
 #' @param n number of observations
 #' @param mu location parameter
-#' @param lambda scale parameter
-#' @param alpha shape parameter
+#' @param scale scale parameter
+#' @param shape shape parameter
 #' @param log,log.p logical; if TRUE, probabilities \code{p} are given as \code{log(p)}.
 #' @param lower.tail logical; if TRUE (default), probabilities are \code{P[X \leq x]}, otherwise,
 #' \code{P[X > x]}.
@@ -16,22 +16,22 @@
 #' @export ddgp pdgp qdgp rdgp
 
 ddgp <-
-function(x, mu=0, lambda=1, alpha=1, log = FALSE) {
+function(x, mu=0, scale=1, shape=1, log = FALSE) {
   # suport is x = mu, mu + 1, mu + 2, ...
   support <- x >= mu
   res <- rep(NA_integer_, length(x))
-  res[support] <- (1 + lambda * (x[support] - mu))**(-alpha) - (1 + lambda * (x[support] - mu + 1))**-alpha
+  res[support] <- (1 + scale * (x[support] - mu))**(-shape) - (1 + scale * (x[support] - mu + 1))**-shape
   res[is.na(res)] <- 0
   res
 }
 
 pdgp <-
-function(q, mu=0, lambda=1, alpha=1, lower.tail = TRUE, log.p = FALSE) {
+function(q, mu=0, scale=1, shape=1, lower.tail = TRUE, log.p = FALSE) {
   
   # find where values of q are less than mu
   zindex <- q < mu
   
-  p <- 1 - (1 + lambda * (q - mu + 1))**-alpha
+  p <- 1 - (1 + scale * (q - mu + 1))**-shape
   
   # replace with 0
   p[zindex] <- 0
@@ -44,17 +44,17 @@ function(q, mu=0, lambda=1, alpha=1, lower.tail = TRUE, log.p = FALSE) {
 }
 
 qdgp <-
-function(p, mu=0, lambda=1, alpha=1, lower.tail = TRUE, log.p = FALSE) {
+function(p, mu=0, scale=1, shape=1, lower.tail = TRUE, log.p = FALSE) {
   if (lower.tail == FALSE) {
     p <- 1 - p
   }
   if (log.p == TRUE) {
     p <- exp(p)
   }
-  ceiling( ((1-p)**(-1/alpha) - 1) / lambda - 1 + mu )
+  ceiling( ((1-p)**(-1/shape) - 1) / scale - 1 + mu )
 }
 
 rdgp <-
-function(n, mu=0, lambda=1, alpha=1) {
-  sample(1:999999, n ,replace = T, prob = ddgp(1:999999, mu, lambda, alpha))
+function(n, mu=0, scale=1, shape=1) {
+  sample(1:999999, n ,replace = T, prob = ddgp(1:999999, mu, scale, shape))
 }
--- R/ZIDGP.R
+++ R/ZIDGP.R
@@ -1,14 +1,14 @@
 #' @name ZIDGPD
 #' @title Zero-inflated Discrete Generalized Pareto Distribution
 #' @description Density, distribution function, quantile function and random generation for the zero-inflated
-#' discrete generalized Pareto distribution with parameters \code{theta}, \code{mu}, \code{lambda}, and \code{alpha}.
+#' discrete generalized Pareto distribution with parameters \code{theta}, \code{mu}, \code{scale}, and \code{shape}.
 #' @param x,q vector of quantiles
 #' @param p vector of probabilities
 #' @param n number of observations
 #' @param theta zero-inflation parameter (probability of zeros)
 #' @param mu location parameter
-#' @param lambda scale parameter
-#' @param alpha shape parameter
+#' @param scale scale parameter
+#' @param shape shape parameter
 #' @param log,log.p logical; if TRUE, probabilities \code{p} are given as \code{log(p)}.
 #' @param lower.tail logical; if TRUE (default), probabilities are \code{P[X \leq x]}, otherwise,
 #' \code{P[X > x]}.
@@ -19,9 +19,9 @@
 ##### TODO Change order and parameter names in description ####
 
 dzidgp <-
-function(x, theta = 0.5, mu = 0, lambda = 1, alpha = 1, log = FALSE) {
+function(x, theta = 0.5, mu = 0, scale = 1, shape = 1, log = FALSE) {
   zeros <- theta * (x == 0)
-  d <- zeros + (1 - theta) * ddgp(x, mu, lambda, alpha)
+  d <- zeros + (1 - theta) * ddgp(x, mu, scale, shape)
   if (log) {
     log(d)
   } else {
@@ -30,8 +30,8 @@ function(x, theta = 0.5, mu = 0, lambda = 1, alpha = 1, log = FALSE) {
 }
 
 pzidgp <-
-function(q, theta = 0.5, mu = 0, lambda = 1, alpha = 1, lower.tail = TRUE, log.p = FALSE) {
-  p <- theta + (1 - theta) * pdgp(q, mu, lambda, alpha)
+function(q, theta = 0.5, mu = 0, scale = 1, shape = 1, lower.tail = TRUE, log.p = FALSE) {
+  p <- theta + (1 - theta) * pdgp(q, mu, scale, shape)
   
   if (lower.tail == FALSE) {
     p <- 1 - p
@@ -45,7 +45,7 @@ function(q, theta = 0.5, mu = 0, lambda = 1, alpha = 1, lower.tail = TRUE, log.p
 }
 
 qzidgp <-
-function(p, theta = 0.5, mu = 0, lambda = 1, alpha = 1, lower.tail = TRUE, log.p = FALSE) {
+function(p, theta = 0.5, mu = 0, scale = 1, shape = 1, lower.tail = TRUE, log.p = FALSE) {
   # does not yet handle multiple values of theta
   
   if (lower.tail == FALSE) {
@@ -58,15 +58,15 @@ function(p, theta = 0.5, mu = 0, lambda = 1, alpha = 1, lower.tail = TRUE, log.p
   }
   pindex <- theta < p
   res <- rep(NA_real_, length(p))
-  res[pindex] <- qdgp((p[pindex] - theta) / (1 - theta), mu, lambda, alpha)
+  res[pindex] <- qdgp((p[pindex] - theta) / (1 - theta), mu, scale, shape)
   res[is.na(res)] <- 0
   res
 }
 
 rzidgp <-
-function(n, theta = 0.5, mu = 0, lambda = 1, alpha = 1) {
+function(n, theta = 0.5, mu = 0, scale = 1, shape = 1) {
   zero   <- rbinom(n, 1, theta)
-  y      <- rdgp(n, mu, lambda, alpha)
+  y      <- rdgp(n, mu, scale, shape)
   output  <- ifelse(zero==1, 0, y)
   output
 }
